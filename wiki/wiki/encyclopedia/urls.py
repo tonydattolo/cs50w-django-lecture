7130894629137f9539702c2d8c02073e6cc5e22a
tdattolo <tdattolo@iu.edu>
2020-11-23 04:20:21 -0500

    Create Entry page, Entry Page plumbing in progress

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+
+# app_name = "encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("createNewPage", views.createNewPage, name="createNewPage"),
+    path("<str:entryName>", views.displayEntryPage, name="displayEntryName")
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,6 +1,19 @@
+import re
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.urls.base import reverse
+
+import markdown2
 
 from . import util
+
+class NewEntryForm(forms.Form):
+    entryTitle = forms.CharField(label="New Entry Title")
+    entryContent = forms.CharField(
+        label="Entry Content in Markdown",
+        widget=forms.Textarea(attrs={"rows":3, "cols":5}))
+    # dateLastEdited = forms.DateField()
 
 
 def index(request):
@@ -8,5 +21,31 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entryPage(request, page):
-    
+def createNewPage(request):
+    # Check if method is POST, or submitting vs viewing new form
+    if request.method =="POST":
+        # Take in data the user submitted and save it as form
+        newEntry = NewEntryForm(request.POST)
+        # Check if form data is valid (server-side)
+        if newEntry.is_valid():
+            # Add new markdown file thus updating list of entries
+            util.save_entry(
+                newEntry.cleaned_data["entryTitle"],
+                newEntry.cleaned_data["entryContent"])
+            return HttpResponseRedirect(reverse("encyclopedia:createNewPage"))
+        
+        # Else, if form has invalid data, re-render with warnings
+        else:
+            return render(request, "encyclopedia/entryPage.html", {
+                "newEntryForm": newEntry
+            })
+    return render(request, "encyclopedia/entryPage.html", {
+        "newEntryForm": NewEntryForm()
+    })
+
+def displayEntryPage(request, entryName):
+    # names = util.list_entries()
+    entryHTML = markdown2.markdown(util.get_entry(entryName))
+    return render(request, "encyclopedia/displayEntry.html", {
+        "entryName": entryHTML
+    })
