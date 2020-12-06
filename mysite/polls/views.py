625c345c79839297f1b810f65b128189f4b78361
tonydattolo <tdattolo@iu.edu>
2020-12-06 01:50:51 -0500

    Results 404 debugging, generic views refactor

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
-from django.http.response import Http404, HttpResponse
+from django.http.response import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Choice, Question
 
@@ -12,21 +13,37 @@ def index(request):
 
 def detail(request, question_id):
     # Shortened version
-    # question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     # syntax     get_object_or_404(Model, model attribute kwargs to check if wanted object exists) uses .get()
     #            get_list_of_404() same but uses .filter and returns list
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
     return render(request, "polls/detail.html", {
         'question': question
     })
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {
+        "question": question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
-
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form 
+        return render(request, 'polls/detail.html', {
+            "question": question,
+            "error_message": "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POSTdara. This prevents data from being posted twice if a 
+        # user hits the back button.
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id)))
