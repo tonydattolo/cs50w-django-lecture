56d845cf35becf28a6d3220046c41d0376218359
tdattolo <tdattolo@iu.edu>
2021-02-25 22:47:15 -0500

    wiki tests

--- wiki/wiki/encyclopedia/tests.py
+++ wiki/wiki/encyclopedia/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from .models import Entry
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+# class SimpleTests(SimpleTestCase):
+#     def testLanding(self):
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+
+class MyTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.Entry = Entry.objects.create(title="testCaseClass")
+        return super().setUpTestData()
+
+    # def test1(self):
+        # some test using self.Entry
+    
+    def testLanding(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
